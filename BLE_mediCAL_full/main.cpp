03056533f12d1da187c5c155a835fb5c0a9b0464
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2014-11-10 08:43:55 -0800

    Commented and Updated code shipped with HW5

--- BLE_mediCAL_full/main.cpp
+++ BLE_mediCAL_full/main.cpp
@@ -7,9 +7,9 @@
 
 DigitalOut led1(LED1);
 BLEDevice  ble;                 // Declare the ble device
-Serial arduino(USBTX, USBRX);   // serial interface to PC or Arduino controlling the stepper
+Serial arduino(USBTX, USBRX);   // serial interface to PC or Arduino controlling the stepper (and debug prints)
 
-// Arbit 128-bit UIDs generated from
+// Arbit 128-bit UIDs generated from 
 // http://www.guidgenerator.com/online-guid-generator.aspx
 // We need one for each service and characteristic
 const uint8_t ARDUINO_WRITE_CHARACTERISTIC_UUID[LENGTH_OF_LONG_UUID] = {
@@ -21,23 +21,34 @@ const uint8_t RFID_UUID[LENGTH_OF_LONG_UUID] = {
     0x7a, 0x77, 0xbe, 0x20, 0x5a, 0x0d, 0x11, 0xe4,
     0xa9, 0x5e, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b
 };
-
+ 
 const uint8_t TEST_SERVICE_UUID[LENGTH_OF_LONG_UUID] = {
     0xb0, 0xbb, 0x58, 0x20, 0x5a, 0x0d, 0x11, 0xe4,
     0x93, 0xee, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b};
 
-const static char DEVICE_NAME[] = "mediCAL BLE";        // For Advertisement
+const static char DEVICE_NAME[] = "mediCAL BLE";        // For Advertisement    
 static volatile uint16_t writeToArduino_handler;        // handler to echo data
 static volatile bool is_button_pressed = false;
 uint8_t RFID_Tag_Read[4];
 
-// Button pressed routine
+// Button1 pressed routine - emulates 'true' RFID UID read
 void button1Pressed() {
-    RFID_Tag_Read[0] = 0xAB;
+    RFID_Tag_Read[0] = 0xAB;   
     RFID_Tag_Read[1] = 0xCD;
     RFID_Tag_Read[2] = 0xEF;
-    RFID_Tag_Read[3] = 0xAB;
+    RFID_Tag_Read[3] = 0x00;
     is_button_pressed = true;
+    arduino.printf("Button 1 Pressed!\n\r");
+}
+
+// Button2 pressed routine - emulates 'true' RFID UID read
+void button2Pressed() {
+    RFID_Tag_Read[0] = 0x12;   
+    RFID_Tag_Read[1] = 0x34;
+    RFID_Tag_Read[2] = 0x56;
+    RFID_Tag_Read[3] = 0x78;
+    is_button_pressed = true;
+    arduino.printf("Button 2 Pressed!\n\r");
 }
 
 // What happens on connect event - DEBUG purposes ONLY!!
@@ -62,7 +73,7 @@ void onDatawritten(const GattCharacteristicWriteCBParams *eventDataP) {
         arduino.printf("Got some Arduino-specific info!\n\r");
         uint16_t bytesRead = eventDataP->len;
         int turnSteps = *((int16_t *)eventDataP->data);
-        arduino.printf("%i\n\r", turnSteps);
+        arduino.printf("%i\n\r", turnSteps); 
         led1 = !led1.read();
     }
 }
@@ -76,26 +87,31 @@ int main(void)
     InterruptIn button1(BUTTON1);
     button1.mode(PullUp);
     button1.rise(&button1Pressed);
+
+    InterruptIn button2(BUTTON2);
+    button2.mode(PullUp);
+    button2.rise(&button2Pressed);
+
     
     // You can write from the phone to control what is written to Arduino
     GattCharacteristic writeToArduino_characteristics(
-                                                      ARDUINO_WRITE_CHARACTERISTIC_UUID, NULL, sizeof(int16_t), sizeof(int16_t),
-                                                      GattCharacteristic::BLE_GATT_FORMAT_SINT16 |                                        // 16-bit signed int
-                                                      GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ |                                 // has read
-                                                      GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE);                                // and write properties
+        ARDUINO_WRITE_CHARACTERISTIC_UUID, NULL, sizeof(int16_t), sizeof(int16_t),
+        GattCharacteristic::BLE_GATT_FORMAT_SINT16 |                                        // 16-bit signed int
+        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ |                                 // has read
+        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE);                                // and write properties
     writeToArduino_handler = writeToArduino_characteristics.getValueAttribute().getHandle();// save the handler
-    
+
     GattCharacteristic RFID_characteristics(
-                                            RFID_UUID, NULL, sizeof(uint32_t), sizeof(uint32_t),
-                                            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ |
-                                            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
-    
-    
-    GattCharacteristic *charTable[] = {&writeToArduino_characteristics,
-        &RFID_characteristics};                    // Characteristic table
+        RFID_UUID, NULL, sizeof(uint32_t), sizeof(uint32_t),
+        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | 
+        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
+
+
+    GattCharacteristic *charTable[] = {&writeToArduino_characteristics, 
+                                                &RFID_characteristics};                    // Characteristic table
     GattService testService(TEST_SERVICE_UUID, charTable,                                   // Add the char(s) to this service
                             sizeof(charTable) / sizeof(GattCharacteristic *));
-    
+
     // BLE setup, mainly we add service and callbacks
     ble.init();
     ble.addService(testService);
@@ -105,24 +121,25 @@ int main(void)
     
     // setup advertising
     ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED |
-                                     GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+                                     GapAdvertisingData::LE_GENERAL_DISCOVERABLE);                      
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME,
                                      (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
     ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
     ble.setAdvertisingInterval(160); /* 100ms; in multiples of 0.625ms. */
     arduino.printf("Start Advertising\n\r");
     ble.startAdvertising();
-    
+
     while (true) {
         if (is_button_pressed) {
             // if button pressed, we update the characteristics
             is_button_pressed = false;
             ble.updateCharacteristicValue(RFID_characteristics.getValueAttribute().getHandle(),
                                           RFID_Tag_Read, sizeof(RFID_Tag_Read));
+          arduino.printf("Characteristic Updated!\n\r");
         } 
         else {
             arduino.printf("Waiting for ble Event\n\r");
             ble.waitForEvent();        
-        }
+            }
     }
 }
--- readMifare_Stepper/readMifare_Stepper.ino
+++ readMifare_Stepper/readMifare_Stepper.ino
@@ -1,39 +1,18 @@
-/**************************************************************************/
-/*! 
-    @file     readMifare.pde
-    @author   Adafruit Industries
-	@license  BSD (see license.txt)
-
-    This example will wait for any ISO14443A card or tag, and
-    depending on the size of the UID will attempt to read from it.
-   
-    If the card has a 4-byte UID it is probably a Mifare
-    Classic card, and the following steps are taken:
-   
-    - Authenticate block 4 (the first block of Sector 1) using
-      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
-    - If authentication succeeds, we can then read any of the
-      4 blocks in that sector (though only block 4 is read here)
-	 
-    If the card has a 7-byte UID it is probably a Mifare
-    Ultralight card, and the 4 byte pages can be read directly.
-    Page 4 is read by default since this is the first 'general-
-    purpose' page on the tags.
-
-
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
-  ----> https://www.adafruit.com/products/364
- 
-Check out the links above for our tutorials and wiring diagrams 
-These chips use SPI to communicate, 4 required to interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-*/
-/**************************************************************************/
+/* Code for HW5 of IDD
+ * 2 basic functions are implemented - RFID reader and stepper driving
+ * Due to time constraint, the RFID reader is implemented in a hacky way:
+ * Only 2 RFID tags are 'stored' in the local Arduino database = circle_tag 
+ * and bart_tag. If circle_tag is read, a digital out (RFID1) is toggled - this
+ * digital out is connected to the nRF51822 to signal a RFID read event
+ * Note that we use Digital out instead of serial to communicate with the 
+ * nRF51822 for legacy purposes - the buttons on the RF board were used 
+ * previously to emulate RFID tag read events, so it was easiest to just
+ * connect to those pins and see if everything worked. In the near future, 
+ * those pins will be removed.
+ * Similarly, if the bart_tag is read, RFID2 is toggled.
+ * The Arduino also relies on the RF baord to tell it how much to spin the
+ * motor
+ */ 
 
 #include <Adafruit_PN532.h>
 #include <Stepper.h>
@@ -43,45 +22,59 @@ products from Adafruit!
 #define SS   (4)
 #define MISO (5)
 
-uint8_t circle[] = {0x1A, 0xE2, 0x41, 0xD9};
-uint8_t bart[] = {0x04, 0x92, 0x6E, 0x7A, 0x7A, 0x31, 0x80};
+/* RFID UIDs - known previously */
+uint8_t circle_tag[] = {0x1A, 0xE2, 0x41, 0xD9};
+uint8_t bart_tag[] = {0x04, 0x92, 0x6E, 0x7A, 0x7A, 0x31, 0x80};
 
 Adafruit_PN532 nfc(SCK, MISO, MOSI, SS);
 
+// RFID pins (SPI)
 int in1Pin = 12;
 int in2Pin = 11;
 int in3Pin = 10;
 int in4Pin = 9;
 
+// hack pins
+int RFID1 = 6;
+int RFID2 = 7;
+
+// motor driver pins (these go to the L293D Hbridge driver)
 Stepper motor(512, in1Pin, in2Pin, in3Pin, in4Pin);  
 
 void setup(void) {
+  /* Stepper pins */
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(in3Pin, OUTPUT);
   pinMode(in4Pin, OUTPUT);
   
+  /* Hack button emulator pins - when either of these 
+     are activated, the RF board writes a particular UID to the
+     BLE notify characteristic.
+   */
+  pinMode(RFID1, OUTPUT);
+  pinMode(RFID2, OUTPUT);
+
   motor.setSpeed(20);
   
   Serial.begin(9600);
-  Serial.println("Hello!");
 
   nfc.begin();
 
+  /* @TODO Verify if this code snippet works properly */
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
     Serial.print("Didn't find PN53x board");
-    while (1); // halt
+    while (1){
+      nfc.begin();
+      versiondata = nfc.getFirmwareVersion();
+      if(versiondata)  break;
+      delay(1);
+    }
   }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
   
   // configure board to read RFID tags
   nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
 }
 
 
@@ -95,100 +88,34 @@ void loop(void) {
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
+  /* If a tag is read successfully, send out the UID to the RF board */
   if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
-    
-    if(isEqual(uid, circle, uidLength)){
-      Serial.print("Circle");
-     Serial.println("");
-      motor.step(100);	  
+    nfc.PrintHex(uid, uidLength);  // print it on serial
+    // but for now, also send it using the hack method
+    if(isEqual(uid, circle_tag, uidLength)){
+        digitalWrite(RFID1, 1);
+        digitalWrite(RFID2, 0);
     }
-    else if(isEqual(uid, bart, uidLength)){
-      Serial.print("Bart");
-     Serial.println("");
-      motor.step(-100);	  
+    else if(isEqual(uid, bart_tag, uidLength)){
+        digitalWrite(RFID1, 0);
+        digitalWrite(RFID2, 1);
     }
-
-    
-    if (uidLength == 4)
-    {
-      // We probably have a Mifare Classic card ... 
-      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
-      // Now we need to try to authenticate it for read/write access
-      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-      Serial.println("Trying to authenticate block 4 with default KEYA value");
-      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-	  
-	  // Start with block 4 (the first block of sector 1) since sector 0
-	  // contains the manufacturer data and it's probably better just
-	  // to leave it alone unless you know what you're doing
-      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-	  
-      if (success)
-      {
-        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
-        uint8_t data[16] = "Jesus is life";
-		
-        // If you want to write something to block 4 to test with, uncomment
-		// the following line and this text should be read back in a minute
-        //success = nfc.mifareclassic_WriteDataBlock(4, data);
-
-        // Try to read the contents of block 4
-        success = nfc.mifareclassic_ReadDataBlock(4, data);
-		
-        if (success)
-        {
-          // Data seems to have been read ... spit it out
-          Serial.println("Reading Block 4:");
-          nfc.PrintHexChar(data, 16);
-          Serial.println("");
-          // Wait a bit before reading the card again
-          delay(1000);
-        }
-        else
-        {
-          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
-        }
-      }
-      else
-      {
-        Serial.println("Ooops ... authentication failed: Try another key?");
-      }
-    }
-    
-    if (uidLength == 7)
-    {
-      // We probably have a Mifare Ultralight card ...
-      Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
-	  
-      // Try to read the first general-purpose user page (#4)
-      Serial.println("Reading page 4");
-      uint8_t data[32];
-      success = nfc.mifareultralight_ReadPage (4, data);
-      if (success)
-      {
-        // Data seems to have been read ... spit it out
-        nfc.PrintHexChar(data, 4);
-        Serial.println("");
-		
-        // Wait a bit before reading the card again
-        delay(1000);
-      }
-      else
-      {
-        Serial.println("Ooops ... unable to read the requested page!?");
-      }
+    else{
+        digitalWrite(RFID1, 0);
+        digitalWrite(RFID2, 0);
     }
   }
+  
+  // listen for motor turn packets
+  if (Serial.available())
+  {
+    int steps = Serial.parseInt();
+    motor.step(steps);
+  }
+  
 }
 
-
+// helper function to check whether two UIDs are equal
 boolean isEqual(uint8_t *ID1, uint8_t *ID2, uint8_t length)
 {
   boolean res = true;
