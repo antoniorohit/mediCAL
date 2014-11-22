531fac37ce605655a2d36460a4519719493266c4
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2014-11-21 17:12:02 -0800

    Modified Stepper example to include BIG 12 V MOTOR as well

--- Stepper_example/Stepper_example.ino
+++ Stepper_example/Stepper_example.ino
@@ -1,7 +1,16 @@
-/*
-Adafruit Arduino - Lesson 16. Stepper
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
 */
 
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
 #include <Stepper.h>
 
 int in1Pin = 12;
@@ -11,8 +20,17 @@ int in4Pin = 9;
 
 Stepper motor(512, in1Pin, in2Pin, in3Pin, in4Pin);  
 
-void setup()
-{
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+
+void setup() {
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(in3Pin, OUTPUT);
@@ -20,7 +38,13 @@ void setup()
   
   Serial.begin(9600);
   motor.setSpeed(20);
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(10);  // 10 rpm   
 }
+
 
 void loop()
 {
@@ -29,4 +53,19 @@ void loop()
     int steps = Serial.parseInt();
     motor.step(steps);
   }
+  Serial.println("Single coil steps");
+  myMotor->step(100, FORWARD, SINGLE); 
+  myMotor->step(100, BACKWARD, SINGLE); 
+
+  Serial.println("Double coil steps");
+  myMotor->step(100, FORWARD, DOUBLE); 
+  myMotor->step(100, BACKWARD, DOUBLE);
+  
+  Serial.println("Interleave coil steps");
+  myMotor->step(100, FORWARD, INTERLEAVE); 
+  myMotor->step(100, BACKWARD, INTERLEAVE); 
+  
+  Serial.println("Microstep steps");
+  myMotor->step(50, FORWARD, MICROSTEP); 
+  myMotor->step(50, BACKWARD, MICROSTEP);
 }
